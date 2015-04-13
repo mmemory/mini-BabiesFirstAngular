74355f9f7258bf82479f4f7bceefce675d37439f
Michael Memory <michaelmemoria@gmail.com>
2015-04-13 13:42:11 -0600

    Finished everything and added styles

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -2,4 +2,9 @@ var app = angular.module('friendsList');
 
 app.controller('mainCtrl', function ($scope) {
     $scope.friends = ['John', 'Mary', 'Steve', 'Tim'];
+
+    $scope.addFriend = function () {
+        $scope.friends.push($scope.name);
+        $scope.name = '';
+    }
 });
